6df52c95b82c4954f4f257ecc7bc54a692879d7a
Zee <xziyad92@gmail.com>
2021-08-20 13:08:17 +0200

    #JS
    22) Create an object with properties such name, surname, email

--- Ex-part-js.js
+++ Ex-part-js.js
@@ -1,8 +1,6 @@
 /*
 JS EXERCISES
 
-        21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
-        22) Create an object with properties such name, surname, email
         23) Delete Email from the previously created object
         24) Create an array with 10 strings in it
         25) Print in the console every string in the previous array
@@ -16,3 +14,10 @@ JS EXERCISES
 let x = "John"
 let y = "Doe"
 console.log(`${x} <> ${y}`)
+
+// 22) Create an object with properties such name, surname, email
+let objectOne = {
+    name: 'Zee',
+    surname: 'Mo',
+    email: 'mozee@example.com'
+}
